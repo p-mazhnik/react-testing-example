0a1f96a7e7a0d59721df5d60762683d71ae5b191
p-mazhnik <pavel.mazhnik@gmail.com>
2020-09-12 15:45:41 +0300

    Step 1: install dependencies, setup testing environment, setup storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <CssBaseline />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
